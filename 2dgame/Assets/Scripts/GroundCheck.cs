70db0645a4abf61e73aa5efc1a70677c1448a61b
Yannis Dejonghe <yannis.dejonghe@hotmail.com>
2018-01-30 20:48:06 +0100

    r

---
+++ 2dgame/Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,5 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour { private Player player; void start() { player = gameObject.GetComponentInParent<Player>(); } void OnTriggerEnter2D(Collider2D col) { if (null == player) { start(); } player.grounded = true; } void OnTriggerExit2D(Collider2D col) { player.grounded = false; } }
